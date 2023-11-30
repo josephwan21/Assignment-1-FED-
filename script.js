a9a3a40e7a4a37e33a4047356852d7d31649b57c
josephwan21 <josephbwanzj@gmail.com>
2023-11-30 12:30:30 +0800

    Changed the variable name "hyperlinks" to a more suitable one

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var hyperlinks = document.querySelectorAll("nav a"); //Selecting all hyperlinks from the html documents to apply the glowing function in
+    var navlinks = document.querySelectorAll("nav a"); //Selecting all hyperlinks from the html documents to apply the glowing function in
 
-    hyperlinks.forEach(function (hyperlink) {
-        hyperlink.addEventListener("mouseenter", function () { //When the mouse cursor hovers over the hyperlink
-            hyperlink.style.transition = "color 0.3s"; //Transition for the hyperlink to change colour when hovered over 
-            hyperlink.style.color = "white"; // Upon hovering mouse on the hyperlinks, it glows white.
+    navlinks.forEach(function (navlink) {
+        navlink.addEventListener("mouseenter", function () { //When the mouse cursor hovers over the hyperlink
+            navlink.style.transition = "color 0.3s"; //Transition for the hyperlink to change colour when hovered over 
+            navlink.style.color = "white"; // Upon hovering mouse on the hyperlinks, it glows white.
         });
 
-        hyperlink.addEventListener("mouseleave", function () { //When mouse leaves the hyperlink
-            hyperlink.style.color = "black"; // Reset color on mouse leave
+        navlink.addEventListener("mouseleave", function () { //When mouse leaves the hyperlink
+            navlink.style.color = "black"; // Reset color on mouse leave
         });
     });
 });
